dba7a7a19ce18ffe6114949dc0b2e5d9b8bd3b5f
alexhwoods <alex@makeswift.com>
2024-05-29 08:04:39 -0400

    add svelte

---
+++ src/components/HelloWorld.svelte
@@ -0,0 +1,14 @@
+<script>
+  // You can add any JavaScript code here if needed
+</script>
+
+<main>
+  <h1>Hello, World!</h1>
+</main>
+
+<style>
+  /* Add any styles for your component here */
+  h1 {
+    color: #ff3e00;
+  }
+</style>
