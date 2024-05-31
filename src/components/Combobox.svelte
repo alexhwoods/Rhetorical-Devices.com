3c3a25ea5c3fc0bbf64fd316c69da182fcec1fd4
alexhwoods <alex@makeswift.com>
2024-05-30 21:57:33 -0400

    --wip-- [skip ci]

--- src/components/Combobox.svelte
+++ src/components/Combobox.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import { Combobox } from "bits-ui";
   import { flyAndScale } from "./transitions";
-  // import { Check, OrangeSlice, CaretUpDown } from "$icons/index.js";
 
   const fruits = [
     { value: "mango", label: "Mango" },
@@ -22,9 +21,20 @@
 
 <Combobox.Root items={filteredFruits} bind:inputValue bind:touchedInput>
   <div class="relative">
-    <!-- <OrangeSlice
+    <svg
       class="absolute start-3 top-1/2 size-6 -translate-y-1/2 text-muted-foreground"
-    /> -->
+      id="Capa_1"
+      enable-background="new 0 0 461.516 461.516"
+      height="512"
+      viewBox="0 0 461.516 461.516"
+      width="512"
+      xmlns="http://www.w3.org/2000/svg"
+      ><g
+        ><path
+          d="m185.746 371.332c41.251.001 81.322-13.762 113.866-39.11l122.778 122.778c9.172 8.858 23.787 8.604 32.645-.568 8.641-8.947 8.641-23.131 0-32.077l-122.778-122.778c62.899-80.968 48.252-197.595-32.716-260.494s-197.594-48.252-260.493 32.716-48.252 197.595 32.716 260.494c32.597 25.323 72.704 39.06 113.982 39.039zm-98.651-284.273c54.484-54.485 142.82-54.486 197.305-.002s54.486 142.82.002 197.305-142.82 54.486-197.305.002c-.001-.001-.001-.001-.002-.002-54.484-54.087-54.805-142.101-.718-196.585.239-.24.478-.479.718-.718z"
+        /></g
+      ></svg
+    >
     <Combobox.Input
       class="inline-flex h-input w-[296px] truncate rounded-9px border border-border-input bg-background px-11 text-sm transition-colors placeholder:text-foreground-alt/50 focus:outline-none focus:ring-2 focus:ring-foreground focus:ring-offset-2 focus:ring-offset-background"
       placeholder="Search a fruit"
