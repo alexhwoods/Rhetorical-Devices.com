c7818527d24a6ec1d63725a1277498d4703f16b2
alexhwoods <alex@makeswift.com>
2024-05-31 00:07:59 -0400

    --wip-- [skip ci]

--- src/components/Combobox.svelte
+++ src/components/Combobox.svelte
@@ -17,6 +17,16 @@
     inputValue && touchedInput
       ? fruits.filter((fruit) => fruit.value.includes(inputValue.toLowerCase()))
       : fruits;
+
+  function handleSelectedChange(selected: { value: string } | undefined) {
+    if (selected) {
+      window.location.href = `/fruits/${selected.value}`;
+    }
+  }
+
+  function handleItemClick(fruit: { value: string }) {
+    window.location.href = `/fruits/${fruit.value}`;
+  }
 </script>
 
 <Combobox.Root
@@ -26,22 +36,24 @@
   onOpenChange={(isOpen) => {
     contentIsOpen = isOpen;
   }}
+  onSelectedChange={handleSelectedChange}
 >
   <div class="relative">
     <svg
-      class="absolute start-4 top-1/2 size-4 -translate-y-1/2 text-muted-foreground"
+      class="absolute left-4 top-1/2 w-4 h-4 -translate-y-1/2 text-muted-foreground"
       id="Capa_1"
       enable-background="new 0 0 461.516 461.516"
       height="512"
       viewBox="0 0 461.516 461.516"
       width="512"
       xmlns="http://www.w3.org/2000/svg"
-      ><g
-        ><path
-          d="m185.746 371.332c41.251.001 81.322-13.762 113.866-39.11l122.778 122.778c9.172 8.858 23.787 8.604 32.645-.568 8.641-8.947 8.641-23.131 0-32.077l-122.778-122.778c62.899-80.968 48.252-197.595-32.716-260.494s-197.594-48.252-260.493 32.716-48.252 197.595 32.716 260.494c32.597 25.323 72.704 39.06 113.982 39.039zm-98.651-284.273c54.484-54.485 142.82-54.486 197.305-.002s54.486 142.82.002 197.305-142.82 54.486-197.305.002c-.001-.001-.001-.001-.002-.002-54.484-54.087-54.805-142.101-.718-196.585.239-.24.478-.479.718-.718z"
-        /></g
-      ></svg
     >
+      <g>
+        <path
+          d="m185.746 371.332c41.251.001 81.322-13.762 113.866-39.11l122.778 122.778c9.172 8.858 23.787 8.604 32.645-.568 8.641-8.947 8.641-23.131 0-32.077l-122.778-122.778c62.899-80.968 48.252-197.595-32.716-260.494s-197.594-48.252-260.493 32.716-48.252 197.595 32.716 260.494c32.597 25.323 72.704 39.06 113.982 39.039zm-98.651-284.273c54.484-54.485 142.82-54.486 197.305-.002s54.486 142.82.002 197.305-142.82 54.486-197.305.002c-.001-.001-.001-.001-.002-.002-54.484-54.087-54.805-142.101-.718-196.585.239-.24.478-.479.718-.718z"
+        />
+      </g>
+    </svg>
     <Combobox.Input
       class="inline-flex h-input w-[504px] truncate rounded-t-3xl {contentIsOpen
         ? 'rounded-b-none'
@@ -58,9 +70,10 @@
     <div class="pt-1 pb-2">
       {#each filteredFruits as fruit (fruit.value)}
         <Combobox.Item
-          class="flex h-10 w-full select-none items-center py-3 pl-9 pr-1.5 text-lg capitalize outline-none font-serif hover:bg-blue-100 rounded-4"
+          class="flex h-10 w-full select-none items-center py-3 pl-9 pr-1.5 text-lg capitalize outline-none font-serif rounded-4 data-[highlighted]:bg-blue-100 hover:bg-blue-100 focus:bg-blue-100"
           value={fruit.value}
           label={fruit.label}
+          on:click={() => handleItemClick(fruit)}
         >
           {fruit.label}
           <Combobox.ItemIndicator class="ml-auto bg-red-500" asChild={false} />
